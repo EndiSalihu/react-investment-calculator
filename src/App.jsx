dfac3d2b3b4bc58cc99135852c1b1d026533d010
Endi Salihu <endi.salihu03@gmail.com>
2024-07-24 19:54:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prev) => ({...prev, [inputIdentifier]: +newValue}))
+  }
+
+
+  const isInputValid = userInput.duration >= 1;
+
+
+
+  return (
+    <>
+      <Header />
+      <UserInput onChangeInput={handleChange} userInput={userInput}/>
+      {isInputValid ? <Results input={userInput} /> : <p className="center">Please enter a duration greater than 0</p>}
+    </>
+  );
+}
+
+export default App;
